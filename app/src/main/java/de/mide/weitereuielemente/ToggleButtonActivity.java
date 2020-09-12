5a4369a87f39b6b9a5243de4583826426259c380
MDecker-MobileComputing <mide_42b@yahoo.de>
2020-09-12 22:47:13 +0200

    ToggleButtonActivity: Umstellung auf Event-Handler-Interface OnCheckedChangeListener (wie in API-Doku beschrieben).

--- app/src/main/java/de/mide/weitereuielemente/ToggleButtonActivity.java
+++ app/src/main/java/de/mide/weitereuielemente/ToggleButtonActivity.java
@@ -4,15 +4,10 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.ProgressBar;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
-import android.widget.SeekBar;
-import android.widget.Toast;
 import android.widget.ToggleButton;
-
+import android.widget.CompoundButton.OnCheckedChangeListener;
 
 /**
  * Activity zur Demonstration des UI-Elements "ToggleButton" (zustandsbehafter Button,
@@ -22,7 +17,7 @@ import android.widget.ToggleButton;
  * This project is licensed under the terms of the BSD 3-Clause License.
  */
 public class ToggleButtonActivity extends Activity
-                                  implements OnClickListener {
+                                  implements OnCheckedChangeListener {
 
     /** UI-Element "Kippschalter". */
     protected ToggleButton _toggleButton = null;
@@ -45,7 +40,7 @@ public class ToggleButtonActivity extends Activity
         _toggleButton = findViewById(R.id.meinToggleButton);
         _progressBar  = findViewById(R.id.meinProgressBar);
 
-        _toggleButton.setOnClickListener(this);
+        _toggleButton.setOnCheckedChangeListener(this);
 
         if ( _toggleButton.isChecked() ) {
 
@@ -59,16 +54,17 @@ public class ToggleButtonActivity extends Activity
 
 
     /**
-     * Event-Handler-Methode für den ToggleButton.<br>
-     * Einzige Methode aus dem Interface {@link OnClickListener}.
+     * Einzige Methode aus dem Event-Handler-Interface {@code OnCheckedChangeListener}.
      *
-     * @param view  UI-Element, welches das Event ausgelöst hat (im vorliegenden
-     *              Fall wird dies immer der ToggleButton sein).
+     * @param buttonView  Button, dessen Zustand geändert wurde.
+     *
+     * @param isChecked  {@code true} genau dann wenn {@code buttonView} nach der
+     *                   Zustandsänderung eingeschaltet ist.
      */
     @Override
-    public void onClick(View view) {
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
 
-        if ( _toggleButton.isChecked() ) {
+        if ( isChecked ) {
 
             _progressBar.setVisibility(View.VISIBLE);
 
